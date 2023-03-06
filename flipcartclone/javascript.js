60bf318280574e061fbabd31494dbacb00537166
PRITI GUPTA <125783687+pritigupta118@users.noreply.github.com>
2023-03-06 13:36:48 +0530

    Add files via upload

---
+++ flipcartclone/javascript.js
@@ -0,0 +1,58 @@
+const product =[
+    {
+        imgurl : "file:///C:/Users/ASUS/OneDrive/Desktop/priti%20c%20programm/.vscode/flipcartclone/card1.webp",
+        Name : "Women's Saree",
+        offer : "Min 50% Off"
+    },
+    {
+        imgurl : "https://rukminim1.flixcart.com/image/400/400/xif0q/shoe/1/i/u/-original-imaggcb7ndjv7fue.jpeg?q=70",
+        Name : "Casual Shoes",
+        offer : "Min 70% Off"
+    },
+    {
+        imgurl : "https://rukminim1.flixcart.com/image/400/400/ks7tuvk0/slipper-flip-flop/m/e/n/10-fe-384wt-384br-footup-multicolor-9-original-imag5u4wjcwzncfh.jpeg?q=70",
+        Name : "Slippers",
+        offer : "Min 70% Off"
+    },
+    {
+        imgurl : "https://rukminim1.flixcart.com/image/400/400/xif0q/showpiece-figurine/j/s/k/7-10-lotus-101-priscilla-17-original-imaghxtqavp3kgz5.jpeg?q=70",
+        Name : "Toran",
+        offer : "Min 50% Off"
+    },
+    {
+        imgurl : "https://rukminim1.flixcart.com/image/400/400/xif0q/fabric/s/f/j/-original-imaggze9yqgerxke.jpeg?q=70",
+        Name : "Women Dress",
+        offer : "Min 70% Off"
+    },
+    {
+        imgurl : "https://rukminim1.flixcart.com/image/400/400/xif0q/ethnic-set/l/r/k/l-sf-white-black-m-soundryafabric-original-imagkv9cwup7azhm.jpeg?q=70",
+        Name : "Men's Ethnic",
+        offer : "Min 70% Off"
+    },
+    {
+        imgurl : "https://rukminim1.flixcart.com/image/400/400/kqjtd3k0/sandal/t/7/u/9-mrj1684-9-aadi-brown-original-imag4jhkctjsefu9.jpeg?q=70",
+        Name : "Shoes",
+        offer : "Min 70% Off"
+    },
+    {
+        imgurl : "https://rukminim1.flixcart.com/image/400/400/xif0q/sweet-mithai/c/n/4/400-agra-petha-agra-famous-petha-sweet-dry-agra-peda-winter-original-imagkcbnhzh2vvbk.jpeg?q=70",
+        Name : "Sweets",
+        offer : "Min 70% Off"
+    },
+    {
+        imgurl : "https://rukminim1.flixcart.com/image/400/400/xif0q/kids-shoe/f/o/w/-original-imaghugwhzzm6amn.jpeg?q=70",
+        Name : "Ethnic Shoes",
+        offer : "Min 70% Off"
+    },
+    {
+        imgurl : "https://rukminim1.flixcart.com/image/400/400/kt39jm80/pooja-thali-set/9/r/e/yes-g-pujathali-r-giftcity-original-imag6gnkpyrmnh7h.jpeg?q=70",
+        Name : "Pooja Thali Sets",
+        offer : "Min 70% Off"
+    },
+    {
+        imgurl : "https://rukminim1.flixcart.com/image/400/400/kyt0ya80/fmcg-combo/x/z/u/valentine-mug-with-chocolates-24-cadbury-original-imagayg7ngbjqgxz.jpeg?q=70",
+        Name : "Festive Gift",
+        offer : "Min 70% Off"
+    },
+    
+]
